2b4d9357406b6a2ebe6184a5481ecf321ee2ef4b
Paul Reichelt <paul.reicheltoutlook.com>
2020-10-13 20:52:43 +0200

    Add styled components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.primary};
+`
+
 function Index() {
-  return <div>Work in progress</div>;
+  return <div>
+    <Title>My page</Title>
+  </div>;
 }
 
 export default Index;
